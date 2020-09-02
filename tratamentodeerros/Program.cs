0f48da7eed08544f9315be3170df9c3fe163d92d
andre <andrefrateschi@gmail.com>
2020-09-02 19:17:39 -0300

    commit 01

---
+++ tratamentodeerros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tratamentodeerros
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int idade;
+
+            try
+            {
+                Console.WriteLine("Digite a sua idade: ");
+
+                idade = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("sua idade: " + idade);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("tratamento de erro");
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
